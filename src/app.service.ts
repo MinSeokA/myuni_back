8fabc4541503a28a6b20c220fe1bcf9129446133
KwonMinSeok22 <vkffkelstm1020@gmail.com>
2024-11-04 01:09:42 +0900

    commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  getHello(): any {
     return {
       message: "My University Application API",
       status: 401, // 401 Unauthorized
