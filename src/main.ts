179a87f31d485bd6fa153ac917dfa67163ca00f0
KwonMinSeok22 <vkffkelstm1020@gmail.com>
2024-11-04 20:56:14 +0900

    commit

--- src/main.ts
+++ src/main.ts
@@ -20,8 +20,8 @@ async function bootstrap() {
     customSiteTitle: 'My University API',
     customfavIcon: 'https://cdn.lunaiz.com/lunaiz_logo/logo-l.svg',
     customJs: [
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui-bundle.min.js',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui-standalone-preset.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui-standalone-preset.js',
     ],
     customCssUrl: [
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui.min.css',
