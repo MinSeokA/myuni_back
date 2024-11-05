6f8719f49fdda8276d06e38fc28f707a054c4300
KwonMinSeok22 <vkffkelstm1020@gmail.com>
2024-11-05 16:33:58 +0900

    commit

--- src/university-application/dto/create-university-application.dto.ts
+++ src/university-application/dto/create-university-application.dto.ts
@@ -9,7 +9,7 @@ export class CreateUniversityApplicationDto {
   applicantUserId: string;
 
   // 대학 JSONB (배열로 구성)
-  @ApiProperty({ description: '대학 지원서' })
+  @ApiProperty({ description: '대학 지원서', type: Array, example: [{ universityId: 'universityId', name: 'name', logo: 'logo', department: 'department', admission: 'admission', applicationType: { isEarlyDecision: true, isRegularDecision: true }, isAdmitted: 'none' }] })
   @IsArray()
   @IsNotEmpty()
   university: Array<{
